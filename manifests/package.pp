834a1d2be1bb63b0957d408cb9ff2558bba15bce
Evgeni Golov <evgeni@golov.de>
2013-04-22 19:55:43 +0400

    allow installing nginx via backports

--- manifests/package.pp
+++ manifests/package.pp
@@ -12,8 +12,21 @@
 #
 class nginx::package (
   $ensure = hiera('ensure', $nginx::params::ensure),
+  $use_backport = hiera('use_backport', $nginx::params::use_backport),
 ) inherits nginx::params {
+  if $use_backport {
+    file { '/etc/apt/preferences.d/nginx.pref':
+      ensure  => $ensure,
+      content => template('nginx/etc/apt/preferences.d/nginx.pref.erb'),
+    }
+  } else {
+    file { '/etc/apt/preferences.d/nginx.pref':
+      ensure  => absent,
+    }
+  }
+
   package { 'nginx':
     ensure => $ensure,
+    require => File['/etc/apt/preferences.d/nginx.pref'],
   }
 }
